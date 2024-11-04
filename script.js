7d979a0bbc76f4a0d661265d7aa62b9951022b82
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-04 14:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const makeChange = (c) => {
   // your name here
+	const result = {
+		q:0,
+		d:0,
+	    n:0,
+		p:0
+	}
+
+	result.q = Math.floor(c/25);
+	c %= 25;
+
+	result.d = Math.floor(c/10);
+	c %= 10;
+
+	result.n = Math.floor(c/5);
+	c %= 5;
+
+	result.p = c
+
+	return result;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c = prompt("Enter c: ")
 alert(JSON.stringify(makeChange(c)));
